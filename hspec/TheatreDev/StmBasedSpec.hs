8bb8074cefcb7104136380ca62a6f880e0aedc8f
Nikita Volkov <nikita.y.volkov@mail.ru>
2023-10-19 23:25:39 +0300

    Add test

--- hspec/TheatreDev/StmBasedSpec.hs
+++ hspec/TheatreDev/StmBasedSpec.hs
@@ -2,7 +2,12 @@
 
 module TheatreDev.StmBasedSpec (spec) where
 
+import qualified Data.IntMap.Strict as IntMap
+import qualified Data.IntSet as IntSet
 import Test.Hspec
+import Test.Hspec.QuickCheck
+import Test.QuickCheck
+import Test.QuickCheck.Monadic
 import qualified TheatreDev.StmBased as Actor
 import Prelude
 
@@ -35,6 +40,7 @@ spec =
         putMVar actorLock ()
 
         takeMVar emitterLock
+        Actor.kill actor
 
         collectedBatches <- reverse . fmap toList <$> readIORef acc
         shouldBe collectedBatches [[1], [2, 3], [4]]
@@ -44,3 +50,37 @@ spec =
 
       it "Kill and wait" do
         pending
+
+    describe "byKeyHash" . modifyMaxSuccess (max 10000) $ do
+      prop "Dispatches individually" $ forAll (chooseInt (0, 99)) $ \size -> forAll arbitrary $ \messages -> monadicIO $ do
+        results <- liftIO do
+          resultQueue <- newTQueueIO
+          actor <-
+            fmap (Actor.byKeyHash id)
+              $ replicateM size
+              $ Actor.spawnStatefulIndividual
+                IntMap.empty
+                ( \state ->
+                    atomically $ writeTQueue resultQueue state
+                )
+                ( \state msg ->
+                    return $ IntMap.alter (Just . maybe 1 succ) msg state
+                )
+
+          for_ @[] messages $ Actor.tell actor
+
+          Actor.kill actor
+          Actor.wait actor
+
+          atomically $ flushTQueue resultQueue
+
+        let allKeys = results >>= IntMap.keys
+            nubbedKeys = nub allKeys
+
+        return
+          $ conjoin
+            [ allKeys === nubbedKeys,
+              if size == 0
+                then nubbedKeys === []
+                else IntSet.fromList nubbedKeys === IntSet.fromList messages
+            ]
