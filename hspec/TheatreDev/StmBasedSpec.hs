84d98024f4e9bc01d05d08e2e12e8e1c84ebd345
Nikita Volkov <nikita.y.volkov@mail.ru>
2023-10-19 23:44:47 +0300

    Highlight the bugs

--- hspec/TheatreDev/StmBasedSpec.hs
+++ hspec/TheatreDev/StmBasedSpec.hs
@@ -1,4 +1,4 @@
-{-# OPTIONS_GHC -Wno-unused-local-binds #-}
+{-# OPTIONS_GHC -Wno-unused-local-binds -Wno-unused-binds #-}
 
 module TheatreDev.StmBasedSpec (spec) where
 
@@ -7,7 +7,6 @@ import qualified Data.IntSet as IntSet
 import Test.Hspec
 import Test.Hspec.QuickCheck
 import Test.QuickCheck
-import Test.QuickCheck.Monadic
 import qualified TheatreDev.StmBased as Actor
 import Prelude
 
@@ -15,7 +14,7 @@ spec :: Spec
 spec =
   do
     describe "spawnStatelessBatched" do
-      let spawnInt step = Actor.spawnStatefulBatched @Int 0 (const (return ())) step
+      let spawnIntUpdater step = Actor.spawnStatefulBatched @Int 0 (const (return ())) step
       let spawnUnit step = Actor.spawnStatefulBatched () (const (return ())) step
 
       it "Works in batches" do
@@ -46,33 +45,98 @@ spec =
         shouldBe collectedBatches [[1], [2, 3], [4]]
 
       it "Threads the state" do
-        pending
+        let input = [0 .. 9]
+            inputLength = length input
+
+        resultsVar <- newTVarIO []
+        actor <- Actor.spawnStatefulBatched [] (const (return ())) $ \state msgs -> do
+          let !newState = foldl' (flip (:)) state msgs
+          atomically $ writeTVar resultsVar newState
+          return newState
+        traverse_ (Actor.tell actor) input
+
+        results <- atomically $ do
+          results <- readTVar resultsVar
+          if length results < inputLength
+            then retry
+            else return results
+
+        shouldBe (reverse results) input
 
       it "Kill and wait" do
-        pending
+        let input = [0 .. 9]
+            inputLength = length input
+
+        resultVar <- newEmptyMVar
+        actor <-
+          Actor.spawnStatefulBatched
+            []
+            ( \state -> do
+                -- threadDelay 1000
+                putMVar resultVar state
+            )
+            ( \state msgs -> return $ foldl' (flip (:)) state msgs
+            )
+        traverse_ (Actor.tell actor) input
+
+        Actor.kill actor
+        Actor.wait actor
+
+        result <- takeMVar resultVar
+        shouldBe result input
+
+    describe "allOf" . modifyMaxSuccess (max 10000) $ do
+      prop "" $ forAll (chooseInt (0, 99)) $ \size -> forAll arbitrary $ \(messages :: [Int]) -> idempotentIOProperty do
+        resultsVar <- newTVarIO []
+        actor <-
+          fmap Actor.oneOf
+            $ replicateM size
+            $ Actor.spawnStatefulIndividual
+              []
+              ( \state ->
+                  atomically
+                    $ modifyTVar' resultsVar
+                    $ mappend state
+              )
+              ( \state msg ->
+                  return $ msg : state
+              )
+
+        for_ messages $ Actor.tell actor
+
+        Actor.kill actor
+        Actor.wait actor
+
+        results <- readTVarIO resultsVar
+
+        return
+          $ conjoin
+            [ length results === (length messages) * size
+            ]
 
     describe "byKeyHash" . modifyMaxSuccess (max 10000) $ do
-      prop "Dispatches individually" $ forAll (chooseInt (0, 99)) $ \size -> forAll arbitrary $ \messages -> monadicIO $ do
-        results <- liftIO do
-          resultQueue <- newTQueueIO
-          actor <-
-            fmap (Actor.byKeyHash id)
-              $ replicateM size
-              $ Actor.spawnStatefulIndividual
-                IntMap.empty
-                ( \state ->
-                    atomically $ writeTQueue resultQueue state
-                )
-                ( \state msg ->
-                    return $ IntMap.alter (Just . maybe 1 succ) msg state
-                )
+      prop "Dispatches individually" $ forAll (chooseInt (0, 99)) $ \size -> forAll arbitrary $ \(messages :: [Int]) -> idempotentIOProperty $ do
+        resultsVar <- newTVarIO []
+        actor <-
+          fmap (Actor.byKeyHash id)
+            $ replicateM size
+            $ Actor.spawnStatefulIndividual
+              IntMap.empty
+              ( \state ->
+                  atomically
+                    $ modifyTVar' resultsVar
+                    $ (:) state
+              )
+              ( \state msg ->
+                  return $ IntMap.alter (Just . maybe 1 succ) msg state
+              )
 
-          for_ @[] messages $ Actor.tell actor
+        for_ messages $ Actor.tell actor
 
-          Actor.kill actor
-          Actor.wait actor
+        Actor.kill actor
+        Actor.wait actor
 
-          atomically $ flushTQueue resultQueue
+        results <- readTVarIO resultsVar
 
         let allKeys = results >>= IntMap.keys
             nubbedKeys = nub allKeys
@@ -84,3 +148,35 @@ spec =
                 then nubbedKeys === []
                 else IntSet.fromList nubbedKeys === IntSet.fromList messages
             ]
+
+-- TODO: Restore it and fix the issue. It's disabled because it's hanging.
+oneOf :: Spec
+oneOf =
+  describe "oneOf" . modifyMaxSuccess (max 10000) $ do
+    prop "" $ forAll (chooseInt (0, 99)) $ \size -> forAll arbitrary $ \(messages :: [Int]) -> idempotentIOProperty do
+      resultsVar <- newTVarIO []
+      actor <-
+        fmap Actor.oneOf
+          $ replicateM size
+          $ Actor.spawnStatefulIndividual
+            []
+            ( \state ->
+                atomically
+                  $ modifyTVar' resultsVar
+                  $ (state <>)
+            )
+            ( \state msg ->
+                return $ msg : state
+            )
+
+      for_ messages $ Actor.tell actor
+
+      Actor.kill actor
+      Actor.wait actor
+
+      results <- readTVarIO resultsVar
+
+      return
+        $ conjoin
+          [ sort results === sort messages
+          ]
