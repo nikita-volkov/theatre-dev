3f61c7f0a0183204435a29d5093bf2b5b2bd2fef
Nikita Volkov <nikita.y.volkov@mail.ru>
2023-11-04 09:25:57 +0300

    Add docs

--- library/TheatreDev/Daemon.hs
+++ library/TheatreDev/Daemon.hs
@@ -14,10 +14,26 @@ where
 import TheatreDev.Prelude
 import TheatreDev.Wait qualified as Wait
 
+-- | Configuration of the daemon behaviour.
 data Config = forall state.
   Config
-  { initialState :: state,
+  { 
+    -- | Initial state of the daemon.
+    initialState :: state,
+    -- | Iteration action, updating the daemon's state.
+    -- It gets executed in a loop,
+    -- with checks of whether the daemon is still alive after each one.
+    -- Killing the daemon will not interrupt the currently ongoing iteration,
+    -- thus providing gracefulness guarantees.
+    -- 
+    -- If an exception is thrown by this action,
+    -- the iteration loop will stop,
+    -- the 'cleanUp' action will get executed and
+    -- in all place where 'wait' is called the exception will be rethrown.
     iterate :: state -> IO state,
+    -- | Clean up after the iteration loop is stopped.
+    -- You can use that to release resources or
+    -- issue notifications about the daemon dying.
     cleanUp :: state -> IO ()
   }
 
@@ -50,6 +66,8 @@ instance Monoid Daemon where
         wait = Wait.all (fmap (.wait) daemons)
       }
 
+-- | Fork a thread to run the daemon loop on
+-- returning immediately with a handle to control it.
 spawn :: Config -> IO Daemon
 spawn Config {..} = do
   iteratingVar <- newTVarIO True
@@ -78,10 +96,19 @@ spawn Config {..} = do
       }
   where
 
+-- | Command the daemon to stop iterating,
+-- finish the ongoing iteration and execute the clean up action.
+--
+-- This action executes immediately.
+-- If you want to block waiting for the daemon to actually die,
+-- after 'kill' you can run 'wait'.
 kill :: Daemon -> IO ()
 kill daemon =
   atomically daemon.kill
 
+-- | Block waiting for the daemon to die either due to getting killed
+-- or due to its iterator action throwing an exception.
+-- The exception will get rethrown here.
 wait :: Daemon -> IO ()
 wait daemon =
   atomically daemon.wait >>= maybe (pure ()) throwIO
